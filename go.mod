e70f60cd822441dc163e00b39579a9d35874e001
Renato Spakauskas <renatospaka@gmail.com>
2021-07-22 21:39:21 -0300

    feat: :tada: start project using fiber http
    fiber already serving

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/renatospaka/go-fiber
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.15.0 // indirect
+	gorm.io/driver/mysql v1.1.1 // indirect
+	gorm.io/gorm v1.21.12 // indirect
+)
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+	app.Get("/", hello)
+
+	app.Listen(":3000")
+}
+
+func hello(c *fiber.Ctx) error {
+	return c.SendString("Welcome to Fiber Http")
+}
